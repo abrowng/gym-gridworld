352e534b6b2edbbfc589408e5f6cc27f696f2ca2
abrowng <abrown.glez@gmail.com>
2019-01-26 23:24:45 -0600

    Random Agent

---
+++ RandomAgent.py
@@ -0,0 +1,17 @@
+import gym
+import gym_gridworld
+import time
+
+env = gym.make('gridworld-v0')
+
+for i in range(100):    #100 episodes
+    env.reset()
+    done = False
+    while done == False:
+        time.sleep(0.05)
+        env.render()
+        state, reward, done, info = env.step(env.action_space.sample())
+    time.sleep(1)
+    print("Terminal state reached: ", state)
+    print("Episode Complete! Reward: ", reward)
+    print("======================================")
--- test.py
+++
@@ -1,6 +0,0 @@
-import gym
-import gym_gridworld
-
-env = gym.make('gridworld-v0')
-
-env.reset()
